b612e3e078a0968e8040e09b1fc78ec88accaf57
Emito Khoza <emito.khoza@gmail.com>
2024-09-08 16:33:20 +0200

    MOre

--- client/app.js
+++ client/app.js
@@ -3,10 +3,15 @@ import { io } from "socket.io-client";
 // Variables
 const socket = io("http://localhost:3000");
 const messageSubmitButton = document.getElementById("submit-message");
+const joinRoomButton = document.getElementById("submit-room-join");
 
 socket.on("connect", () => {
   displayMessage(`You connected with id: ${socket.id}`);
   //   socket.emit("custom-event", 10, "Hi", { a: "A" });
+});
+
+socket.on("receive-message", (message) => {
+  displayMessage(message.message);
 });
 
 function displayMessage(message) {
@@ -23,10 +28,23 @@ messageSubmitButton.addEventListener("click", ($event) => {
   $event.preventDefault();
 
   const messageInput = document.getElementById("message-input");
+  const roomInput = document.getElementById("room-input");
+  displayMessage(`Me: ${messageInput.value}`);
 
   socket.emit("send-message", {
     message: messageInput.value,
+    room: roomInput.value,
   });
 
   messageInput.value = "";
+});
+
+joinRoomButton.addEventListener("click", ($event) => {
+  $event.preventDefault();
+
+  const roomInput = document.getElementById("room-input");
+
+  socket.emit("join-room", roomInput.value);
+
+  displayMessage(`You have joined room ${roomInput.value}`);
 });
